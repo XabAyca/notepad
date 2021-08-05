931f997877a73eba2354d3cf40b80bce0dbc6d82
Xabi <xabi.aycaguer@gmail.com>
2021-08-05 17:06:55 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import AllNotes from 'components/AllNotes';
+import Main from 'components/Main';
+import React from 'react';
+import uuid from 'react-uuid';
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = React.useState(JSON.parse(localStorage.notes) || []);
+  const [activeNote, setActiveNote] = React.useState(false)
+  
+  React.useEffect(() => {
+    localStorage.setItem("notes",JSON.stringify(notes))
+  }, [notes])
+
+  const onAddNote = () => {
+    const newNote = { 
+      id: uuid(),
+      title: "Untitled Note",
+      body:"",
+      lastModified: Date.now()
+    }
+    setNotes([newNote, ...notes ])
+  }
+
+  const onUpdatedNote = (updatedNote) => {
+    const updatedNotesArray = notes.map((note) => {
+      if (note.id === activeNote) {
+        return updatedNote
+      }
+      return note
+    })
+    setNotes(updatedNotesArray)
+  }
+
+  const onDeleteNote = (idToDelete) => {
+    setNotes(notes.filter((note)=> note.id !== idToDelete))
+  }
+  
+  const getActiveNote = () => {
+    return notes.find(note => note.id === activeNote)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AllNotes
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdatedNote={onUpdatedNote}/>
     </div>
-  );
+  )
 }
 
 export default App;
